7c9bc1db9b22ae77e31b823e4d02f5c8219828f5
Gary Berkinfand <gberkinfand@gmail.com>
2021-01-15 00:30:02 +0200

    Added recipe model.

--- app/core/models.py
+++ app/core/models.py
@@ -24,6 +24,7 @@ class UserManager(BaseUserManager):
 
         return user
 
+
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that supports using email instead of username"""
     email = models.EmailField(max_length=255, unique=True)
@@ -47,6 +48,7 @@ class Tag(models.Model):
     def __str__(self):
         return self.name
 
+
 class Ingredient(models.Model):
     """Ingredient to be used in a recipe"""
     name = models.CharField(max_length=255)
@@ -59,4 +61,18 @@ class Ingredient(models.Model):
         return self.name
 
 
+class Recipe(models.Model):
+    """Recipe object"""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=255)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+    ingredients = models.ManyToManyField('Ingredient')
+    tags = models.ManyToManyField('Tag')
 
+    def __str__(self):
+        return self.title
